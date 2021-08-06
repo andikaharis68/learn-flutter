37e514dc7df3e505bd3d4f923a800858f05021bc
andikaharis68 <andikaharis68@gmail.com>
2021-08-06 15:11:04 +0700

    making folder model

---
+++ to_do_flutter/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_flutter/screen/to_do_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ToDoScreen(),
+    );
+  }
+}
---
+++ to_do_flutter/lib/model/to_do.dart
@@ -0,0 +1,7 @@
+class Todo {
+  int id;
+  String todo;
+  String description;
+
+  Todo(this.id, this.todo, this.description);
+}
